a4618b2ab3a4e8565c99aa994186c0116061fff3
Matthew Bestard <matthew.r.bestard@gmail.com>
2019-04-05 17:45:27 -0400

    Practice of data input and output using Console.Write, string variables and Console.WriteLine.

--- practice_csharp_application/Program.cs
+++ practice_csharp_application/Program.cs
@@ -10,12 +10,9 @@ namespace practice_csharp_application
     {
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
-
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+            Console.Write("What is your name?: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello " + name);
         }
     }
 }
